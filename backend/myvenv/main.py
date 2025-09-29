92c7771dd997ed661160eca50b7b58289ba599a9
Anwesh-18 <cse.24bcsh15@silicon.ac.in>
2025-09-29 18:04:52 +0530

    added basic backend with db connection

--- backend/myvenv/main.py
+++
@@ -1,9 +0,0 @@
-# from typing import Union
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-
-app = FastAPI()
-
-@app.get("/")
-def redirect_react():
-    return RedirectResponse(url="http://localhost:3000/")
